81fb664aa885c01f15f789dc23b72d1ceef4098f
Linuka Rivi Rihan <linukarivirihan@gmail.com>
2024-09-07 21:05:00 +0530

    working with MVC

---
+++ Controllers/ServicesController.cs
@@ -0,0 +1,24 @@
+﻿using aspnet_app.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_app.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            var service = new VehicleService();
+            service.Id = 1;
+            service.Title = "Oil Change";
+            service.Description = "Change the oil in your vehicle";
+            service.Cost = 50.00m;
+
+            return View(service);
+        }
+    }
+}
---
+++ Models/VehicleService.cs
@@ -0,0 +1,11 @@
+﻿namespace aspnet_app.Models
+{
+    public class VehicleService
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Cost { get; set; }
+        public bool IsDeleted { get; set; } = false;
+    }
+}
